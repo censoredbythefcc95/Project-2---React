fe2680793ae16cda64cc32fa942cee27a7ae1d2c
Your Name <sastudillo6@gmail.com>
2021-01-20 13:03:31 -0500

    Added my main two routers and confirmed they are properly working

--- src/App.js
+++ src/App.js
@@ -2,15 +2,23 @@ import React from "react";
 //Importing Router features
 import { Route } from "react-router-dom";
 
+//Components being rendered into app.js
 import './App.css';
-import SearchBar from "./components/SearchBar";
-import Footer from "./components/Footer";
+import LandingText from "./components/LandingText";
+import MainDisplay from "./components/MainDisplay";
 
 function App() {
   return (
     <div className="App">
-      <SearchBar />
-      <Footer />
+      <Route path="/">
+        <LandingText />
+      </Route>
+      <Route path="/weather">
+        <MainDisplay />
+      </Route>
+
+
+      
     </div>
   );
 }
---
+++ src/components/MainDisplay.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const MainDisplay = () => {
+    return (
+        <div className="MainDisplay">
+            <h3>This is the Main Display Component</h3>
+        </div>
+    )
+}
+
+export default MainDisplay;
