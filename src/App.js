eb42c470082afb6da03a59749d932d603938df06
Your Name <sastudillo6@gmail.com>
2021-01-20 12:49:47 -0500

    Enabled router features and added MainDisplay component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
+import React from "react";
+//Importing Router features
+import { Route } from "react-router-dom";
+
 import './App.css';
 import SearchBar from "./components/SearchBar";
-import Location from "./components/Location";
 import Footer from "./components/Footer";
-import HighLow from "./components/HighLow";
-import CurrentTemperature from "./components/CurrentTemperature";
-import LandingText from "./components/LandingText";
 
 function App() {
   return (
     <div className="App">
       <SearchBar />
-      <Location />
       <Footer />
-      <HighLow />
-      <CurrentTemperature />
-      <LandingText />
     </div>
   );
 }
