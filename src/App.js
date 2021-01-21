82ea00a3627d664bad554167595a6d83e216eb6e
Your Name <sastudillo6@gmail.com>
2021-01-21 11:15:58 -0500

    Added Route component for App to allow history.push command to work

--- src/App.js
+++ src/App.js
@@ -7,12 +7,12 @@ import './App.css';
 import LandingText from "./components/LandingText";
 import MainDisplay from "./components/MainDisplay";
 import SearchBar from "./components/SearchBar";
-
+//Setting our State in function App
 function App(props) {
   const [weather, setWeather] = useState({});
   const [zip, setZip] = useState("");
 
-
+//Fetching API call and listening to the search bar
 const handleSubmit =  async (e) => {
   e.preventDefault();
 
@@ -21,11 +21,11 @@ const handleSubmit =  async (e) => {
     const checkweather = await response.json();
     setWeather(checkweather);
     console.log(weather);
+    props.history.push("/weather");
 
     //props history push causing a typerror. Consult with team lead tomorrow
 
 }
-
 
 
   return (
--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 //Importing Router into index.js
-import {BrowserRouter as Router} from "react-router-dom";
+import {BrowserRouter as Router, Route} from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <App />
+     <Route component={App} path="/" />
     </Router>
   </React.StrictMode>,
   document.getElementById('root')
