8bc97cb7e730aa1af8c8abd9a44713ad6e85cb0f
Your Name <sastudillo6@gmail.com>
2021-01-22 10:12:12 -0500

    Navbar completed, finalizing styling. Project has met MVP at this time

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,6 +4,7 @@ const NavBar = () => {
     return (
         <div>
         <div className="nav-bar">
+            <h3>SunorRain</h3>
             <ul className="container">
                 <li><a href="/">Home</a></li>
                 <li><a href="/">About This App</a></li>
